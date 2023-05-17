ea29f95c1a30223ad539aa58219ab437a404fa04
Bharath Kumar <68462057+BharathBK05@users.noreply.github.com>
2023-05-17 06:45:00 +0000

    Testing

--- index.js
+++ index.js
@@ -2,19 +2,13 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-const getDirectories = (source) =>
-  fs
-    .readdirSync(source, { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => dirent.name);
-
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 const port = 9000;
 
 app.use("/", (req, res) => {
-  res.render("hel");
+  res.render("home");
 });
 
 app.listen(9000, () => {
