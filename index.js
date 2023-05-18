22d0ef822781b8fa6ef2f694935c78580d7aad65
Balasubramani R <46841670+balu8056@users.noreply.github.com>
2023-05-18 19:41:00 +0530

    summa

--- index.js
+++ index.js
@@ -1,16 +1,150 @@
+// const express = require("express");
+// const path = require("path");
+// const fs = require("fs");
+
+// const app = express();
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
+// const port = 9000;
+
+// app.use("/", (req, res) => {
+//   res.render("home");
+// });
+
+// app.listen(9000, () => {
+//   console.log(`Starting Server on Port ${port}`);
+// });
+
+
+//jshint esversion:6
+
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const _ = require("lodash");
 const path = require("path");
-const fs = require("fs");
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-const port = 9000;
+app.set('view engine', 'ejs');
 
-app.use("/", (req, res) => {
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+mongoose.connect("mongodb+srv://admin-bharath:14362348@cluster0.lisc3.mongodb.net/portfolioDB", {useNewUrlParser : true});
+
+const potfolioSchema = {
+    name : String,
+    email : String,
+    phone : Number,
+    messsage : String
+}
+
+const portfolio = mongoose.model("portfolio", potfolioSchema);
+
+
+app.get("/", function(req,res){
   res.render("home");
 });
 
-app.listen(9000, () => {
-  console.log(`Starting Server on Port ${port}`);
+app.get("/about",function(req,res){
+  res.render("about");
+});
+
+app.get("/experience",function(req,res){
+  res.render("experience");
+});
+
+app.get("/contact",function(req,res){
+  res.render("contact");
+});
+
+
+app.post("/contact", function(req,res){
+    const data = (
+    {
+        name : req.body.Name,
+        email : req.body.Email,
+        phone : req.body.Number,
+        messsage : req.body.Message
+    }
+    )
+
+    portfolio.insertMany(data, function(err){
+        if(err){
+            console.log("error")
+        }
+        else{
+            console.log("Saved Successfully")
+        }
+    })
+
+    res.render("contact");
+
+  });
+
+//Blog
+
+const blogSchema = {
+  title : String,
+  content : String
+};
+
+const Post = mongoose.model("Post", blogSchema);
+
+
+app.get("/blogHome",function(req,res){
+  Post.find({},function(err,posts)
+  {
+    res.render("blogHome",{
+      posts : posts
+      
+    });
+
+  });
+  
+});  
+
+app.get("/compose",function(req,res){
+  res.sendFile(__dirname+ "/views/compose.html");
+});
+
+app.post("/compose" ,function(req,res){
+  const post = new Post ({
+    title : req.body.postTitle,
+    content : req.body.postBody
+  });
+
+  post.save(function(err)
+  {
+    if(!err)
+    {
+      res.redirect("/");
+    }
+  });
+
+  
+});
+
+app.get("/posts/:postId", function(req, res){
+
+  const requestedPostId = req.params.postId;
+  
+    Post.findOne({_id: requestedPostId}, function(err, post){
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    });
+  
+  });
+
+
+let port = process.env.PORT;
+
+
+app.listen(port || 9999, function() {
+  console.log("Server has started successfully");
 });
